47ea2b82261e2c00726d24c08367f0d2ce1e7669
Jacob Wilkins <jacobwilkins501@gmail.com>
2018-01-07 22:22:46 -0600

    Added Player Sprites and animated player movement

---
+++ Assets/Rotate_Arm.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Arm : MonoBehaviour {
+
+	public int rotOffset = 90;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 diff = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		diff.Normalize ();
+
+		float rotZ = Mathf.Atan2 (diff.y, diff.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ + rotOffset);
+	}
+}

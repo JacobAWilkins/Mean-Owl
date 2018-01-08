425ad2b7b274ceb0069cada40af8428203138c17
Jacob Wilkins <jacobwilkins501@gmail.com>
2018-01-08 13:55:21 -0600

    Began editing Player script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
 
 	public class PlayerStats {
-		public float Health = 100f;
+		public int Health = 100f;
+	}
+
+	public PlayerStats playerStats = new PlayerStats ();
+
+	public void DamagePlayer (int damage) {
+		playerStats.Health -= damage;
+		if (playerStats <= 0) {
+			
+		}
 	}
 
 }

bc1ccec5afe94bc45d956a9afe95790346fbf4b1
Jacob Wilkins <jacobwilkins501@gmail.com>
2018-01-10 16:43:18 -0600

    Added status indicator to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,41 @@ public class Enemy : MonoBehaviour {
 
 	[System.Serializable]
 	public class EnemyStats {
-		public int Health = 100;
+		public int maxHealth = 100;
+		private int _currentHealth;
+
+		public int currentHealth {
+			get { return _currentHealth; }
+			set { _currentHealth = Mathf.Clamp (value, 0, maxHealth); }
+		}
+
+		public void Init () {
+			currentHealth = maxHealth;
+		}
 	}
 
 	public EnemyStats enemyStats = new EnemyStats ();
-	public int fallBoundary = -20;
+
+	[SerializeField]
+	private StatusIndicator statusIndicator;
+
+	void Start () {
+		enemyStats.Init ();
+
+		if (statusIndicator != null) {
+			statusIndicator.SetHealth (enemyStats.currentHealth, enemyStats.maxHealth);
+		}
+	}
 
 	public void DamageEnemy (int damage) {
-		enemyStats.Health -= damage;
-		if (enemyStats.Health <= 0) {
+		enemyStats.currentHealth -= damage;
+
+		if (enemyStats.currentHealth <= 0) {
 			GameControl.KillEnemy (this);
+		}
+
+		if (statusIndicator != null) {
+			statusIndicator.SetHealth (enemyStats.currentHealth, enemyStats.maxHealth);
 		}
 	}
 }
---
+++ Assets/StatusIndicator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusIndicator : MonoBehaviour {
+
+	[SerializeField]
+	private RectTransform healthBar;
+
+	public void SetHealth (int _current, int _max) {
+		float _value = (float)_current / _max;
+		healthBar.localScale = new Vector3 (_value, healthBar.localScale.y, healthBar.localScale.z);
+	}
+}
